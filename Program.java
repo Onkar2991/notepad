3584bab3d8772f9a5fb7f2b880346366def2147f
Onkar2991 <138042599+Onkar2991@users.noreply.github.com>
2023-12-05 11:58:23 +0530

    commit

---
+++ Program.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Program 
+{
+ public static void main (String args[])
+ {
+   //String str = "abcddef";
+ 
+  List<String> list = new ArrayList<>();
+
+  list.add("abcddef");
+
+  System.out.println(list);   
+   
+  //String op= list.stream()
+	
+   
+   
+   
+   
+
+ }
+
+}
